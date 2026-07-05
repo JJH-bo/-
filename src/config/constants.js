3f88b58b190f170bb77c725990d3ff855f86ff8c
JJH-bo <2691587086@qq.com>
2026-07-05 21:03:08 +0800

    V6.3 camera presets for Manhattan island

--- src/config/constants.js
+++ src/config/constants.js
@@ -16,19 +16,23 @@ export const MODES = {
 };
 
 export const CAMERA_PRESETS = {
-  overview: { position: [980, 760, 1080], target: [60, 0, 60] },
-  entrance: { position: [-850, 330, 520], target: [-630, 0, 245] },
-  queens: { position: [-770, 430, 230], target: [-455, 0, -60] },
-  brooklyn: { position: [140, 440, 720], target: [145, 0, 355] },
-  manhattan: { position: [560, 520, 280], target: [155, 0, -140] },
-  bay: { position: [850, 410, 520], target: [560, 0, 270] },
-  risk: { position: [-120, 330, 820], target: [-165, 0, 560] }
+  overview: { position: [980, 790, 1110], target: [-8, 0, 20] },
+  entrance: { position: [-650, 360, 930], target: [-220, 0, 590] },
+  concept: { position: [-650, 360, 930], target: [-220, 0, 590] },
+  first: { position: [520, 430, 760], target: [-10, 0, 345] },
+  reduction: { position: [250, 420, 530], target: [-105, 0, 120] },
+  manhattan: { position: [530, 520, 320], target: [28, 0, -98] },
+  linear: { position: [530, 520, 320], target: [28, 0, -98] },
+  geo: { position: [500, 430, -620], target: [-8, 0, -380] },
+  bay: { position: [520, 420, 420], target: [165, 0, 92] },
+  physics: { position: [520, 420, 420], target: [165, 0, 92] },
+  risk: { position: [640, 330, 1010], target: [300, 0, 650] }
 };
 
 export const LABEL_RULES = {
-  NEAR_HIDE: 420,
-  DISTRICT_MIN: 520,
-  LANDMARK_MIN: 520,
-  LANDMARK_MAX: 1540,
-  MID_MAX: 1080
+  NEAR_HIDE: 360,
+  DISTRICT_MIN: 500,
+  LANDMARK_MIN: 420,
+  LANDMARK_MAX: 1500,
+  MID_MAX: 980
 };
